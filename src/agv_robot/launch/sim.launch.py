103d93a227c62841d72fdabe1c5a88a5b144b67a
sheelscripts <sheelscripts@gmail.com>
2026-09-02 09:37:54 +0530

    B3: nav2 params and launch file added

--- src/agv_robot/launch/sim.launch.py
+++ src/agv_robot/launch/sim.launch.py
@@ -1,8 +1,12 @@
 import os
+# pyrefly: ignore [missing-import]
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+# pyrefly: ignore [missing-import]
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
+# pyrefly: ignore [missing-import]
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+# pyrefly: ignore [missing-import]
 from launch_ros.actions import Node
 import xacro
 
